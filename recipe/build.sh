ccf62efcadef2150d0afbfe258efc56e21d3a167
Min RK <benjaminrk@gmail.com>
2017-04-18 13:38:45 +0200

    parallel build

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,6 @@
 # The without snapshot comes from the error in
 # https://github.com/nodejs/node/issues/4212.
 ./configure --prefix=$PREFIX --without-snapshot
-make
+make -j${CPU_COUNT}
 make install
 
