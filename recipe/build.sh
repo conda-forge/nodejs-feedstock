7c0dcae26b4a02dc125c46ff50737b093e098a6f
Min RK <benjaminrk@gmail.com>
2017-06-19 10:47:51 +0200

    upgrade and dedupe npm
    might help stave off long path issues on Windows
    
    at least where the cutoff was close.

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,4 +8,12 @@ make install
 
 node -v
 npm version
+# upgrade npm
+npm install -g npm@5.0
+# dedupe npm itself to get some slight protection from long paths on Windows
+# Unclear why npm doesn't do this by default
+# when it does for packages other than itself.
+cd $PREFIX/lib/node_modules/npm
+npm dedupe
 
+npm version
