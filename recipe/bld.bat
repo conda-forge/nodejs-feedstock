98456e62a2544d401093fe10aceb6efd8aa24ea1
Min RK <benjaminrk@gmail.com>
2017-11-23 21:36:28 +0100

    dedupe npm on Windows
    makes too-long path errors slightly less likely

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,4 +8,8 @@ call vcbuild.bat nosign release %PLATFORM%
 
 COPY Release\node.exe %LIBRARY_BIN%\node.exe
 
+%LIBRARY_BIN%\node.exe -v
 %LIBRARY_BIN%\node.exe deps\npm\bin\npm-cli.js install npm -gf
+REM dedupe to avoid too-long path issues on Windows
+%LIBRARY_BIN%\npm dedupe
+%LIBRARY_BIN%\npm version
