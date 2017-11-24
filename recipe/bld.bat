b179d1a6af42ad4ba56dbc9024991acbf1c9c9ac
Min RK <benjaminrk@gmail.com>
2017-11-24 20:12:23 +0100

    install local npm on Windows
    instead of by name, which could get newer version over time

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,7 +9,7 @@ call vcbuild.bat nosign release %PLATFORM%
 COPY Release\node.exe %PREFIX%\node.exe
 
 %PREFIX%\node.exe -v
-%PREFIX%\node.exe deps\npm\bin\npm-cli.js install npm -gf
+%PREFIX%\node.exe deps\npm\bin\npm-cli.js install deps\npm -gf
 %PREFIX%\npm.cmd version
 REM dedupe npm to avoid too-long path issues on Windows
 cd %PREFIX%\node_modules\npm
