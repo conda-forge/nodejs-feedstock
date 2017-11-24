e956a365088bf6944796a6c1af0d7c5bd983f2cc
Min RK <benjaminrk@gmail.com>
2017-11-24 12:51:04 +0100

    npm is npm.cmd
    PATHEXT cannot be trusted

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,5 +11,5 @@ COPY Release\node.exe %LIBRARY_BIN%\node.exe
 %LIBRARY_BIN%\node.exe -v
 %LIBRARY_BIN%\node.exe deps\npm\bin\npm-cli.js install npm -gf
 REM dedupe to avoid too-long path issues on Windows
-%LIBRARY_BIN%\npm dedupe
-%LIBRARY_BIN%\npm version
+%LIBRARY_BIN%\npm.cmd dedupe
+%LIBRARY_BIN%\npm.cmd version
