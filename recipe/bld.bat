0b3d17b5377836249fad92900d058e1e97c2b5f1
Min RK <benjaminrk@gmail.com>
2017-11-24 20:20:53 +0100

    copy build & dedupe from master

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,6 +6,11 @@ if "%ARCH%"=="32" (
 
 call vcbuild.bat nosign release %PLATFORM%
 
-COPY Release\node.exe %LIBRARY_BIN%\node.exe
+COPY Release\node.exe %PREFIX%\node.exe
 
-%LIBRARY_BIN%\node.exe deps\npm\cli.js install npm -gf
+%PREFIX%\node.exe -v
+%PREFIX%\node.exe deps\npm\cli.js install deps\npm -gf
+%PREFIX%\npm.cmd version
+REM dedupe npm to avoid too-long path issues on Windows
+cd %PREFIX%\node_modules\npm
+%PREFIX%\npm.cmd dedupe
