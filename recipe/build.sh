f736b2baf53e32107e91c1794c546eea884f735a
Min RK <benjaminrk@gmail.com>
2018-10-24 10:49:16 +0200

    try removing -std=c++17
    command failing with a syntax error has both `-std=gnu++1y` from node and `-std=c++17` from conda-build,
    so a conflict there is my first guess.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# remove `-std=c++17` from CXXFLAGS
+export CXXFLAGS=$(echo $CXXFLAGS | sed -E 's@\-std=[^ ]+@@')
 # The without snapshot comes from the error in
 # https://github.com/nodejs/node/issues/4212.
 ./configure --prefix=$PREFIX --without-snapshot
