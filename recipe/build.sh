0fe9c35e5ce453e3d87329b6290377a0249a5f38
Min RK <benjaminrk@gmail.com>
2018-12-17 10:48:11 +0100

    scrub -std= flag from cxxflags
    nodejs sets its own `-std` flag, respect it.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# scrub -std=... flag which conflicts with builds
+export CXXFLAGS=$(echo ${CXXFLAGS:-} | sed -E 's@\-std=[^ ]*@@g')
+
 # The without snapshot comes from the error in
 # https://github.com/nodejs/node/issues/4212.
 ./configure --prefix=$PREFIX --without-snapshot
