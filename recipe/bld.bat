3baf682f4049a0d97bb5b5ac70ac188a68a0fbcf
Min RK <benjaminrk@gmail.com>
2019-02-18 13:47:08 +0100

    redistribute binary node.js on Windows
    instead of compiling from source
    
    still compile from source on non-Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,19 +1,6 @@
-if "%ARCH%"=="32" (
-   set PLATFORM=x86
-) else (
-  set PLATFORM=x64
-)
-:: Remove -GL from CXXFLAGS as whole program optimization takes too much time and memory
-set "CFLAGS= -MD"
-set "CXXFLAGS= -MD"
-
-call vcbuild.bat nosign nosnapshot no-cctest release %PLATFORM%
-
-COPY Release\node.exe %PREFIX%\node.exe
-
-%PREFIX%\node.exe -v
-%PREFIX%\node.exe deps\npm\bin\npm-cli.js install deps\npm -gf
-cmd /c %PREFIX%\npm.cmd version
-REM dedupe npm to avoid too-long path issues on Windows
-cd %PREFIX%\node_modules\npm
-cmd /c %PREFIX%\npm.cmd dedupe
+COPY node.exe %PREFIX%\node.exe
+COPY npm %PREFIX%\npm
+COPY npm.cmd %PREFIX%\npm.cmd
+COPY npx %PREFIX%\npx
+COPY npx.cmd %PREFIX%\npx.cmd
+ROBOCOPY node_modules %PREFIX%\node_modules /s /e
