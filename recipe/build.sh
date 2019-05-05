8994d230485e44abeeefc81a3f38cb95dc0f26aa
Josh Reichardt <josh.reichardt@gmail.com>
2019-05-05 08:31:10 -0700

    Try different number of CPUs

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,10 @@
 # scrub -std=... flag which conflicts with builds
 export CXXFLAGS=$(echo ${CXXFLAGS:-} | sed -E 's@\-std=[^ ]*@@g')
 
+# Override default number of CPUs
+echo "Using $(nproc) CPUs"
+CPU_COUNT=$(nproc)
+
 # The without snapshot comes from the error in
 # https://github.com/nodejs/node/issues/4212.
 ./configure --prefix=$PREFIX --without-snapshot
