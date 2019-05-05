d659223ef5fa26a5312389f2aab463f43203b3e1
Josh Reichardt <josh.reichardt@gmail.com>
2019-05-05 09:12:11 -0700

    Try different number of CPUs

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,11 @@
 # scrub -std=... flag which conflicts with builds
 export CXXFLAGS=$(echo ${CXXFLAGS:-} | sed -E 's@\-std=[^ ]*@@g')
 
-# Override default number of CPUs
-echo "Using $(nproc) CPUs"
-CPU_COUNT=$(nproc)
+# Increase CPUs for multiarch builds
+if [ "$(uname -m)" = "armv8" ] || [ "$(uname -m)" = "ppc64le" ]; then
+    echo "Using $(grep -c ^processor /proc/cpuinfo) CPUs"
+    CPU_COUNT=$(grep -c ^processor /proc/cpuinfo)
+fi
 
 # The without snapshot comes from the error in
 # https://github.com/nodejs/node/issues/4212.
