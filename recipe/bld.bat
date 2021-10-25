5566ab22157aea4437f0f4d8835beade612f7646
Uwe L. Korn <uwe.korn@quantco.com>
2021-10-25 15:46:28 +0200

    More explicit Windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,13 @@
+@echo on
 COPY node.exe %PREFIX%\node.exe
+if errorlevel 1 exit 1
 COPY npm %PREFIX%\npm
+if errorlevel 1 exit 1
 COPY npm.cmd %PREFIX%\npm.cmd
+if errorlevel 1 exit 1
 COPY npx %PREFIX%\npx
+if errorlevel 1 exit 1
 COPY npx.cmd %PREFIX%\npx.cmd
+if errorlevel 1 exit 1
 ROBOCOPY node_modules %PREFIX%\node_modules /s /e
-if %ERRORLEVEL% LSS 8 exit 0
+if errorlevel 1 exit 1
