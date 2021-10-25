aba7fa51f49c076a2186b8bd7101cd942c05b7d2
Uwe L. Korn <uwe.korn@quantco.com>
2021-10-25 16:23:55 +0200

    Add reporting

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,4 +10,21 @@ if errorlevel 1 exit 1
 COPY npx.cmd %PREFIX%\npx.cmd
 if errorlevel 1 exit 1
 ROBOCOPY node_modules %PREFIX%\node_modules /s /e
+if %ERRORLEVEL% EQU 16 echo ***FATAL ERROR*** & goto end
+if %ERRORLEVEL% EQU 15 echo OKCOPY + FAIL + MISMATCHES + XTRA & goto end
+if %ERRORLEVEL% EQU 14 echo FAIL + MISMATCHES + XTRA & goto end
+if %ERRORLEVEL% EQU 13 echo OKCOPY + FAIL + MISMATCHES & goto end
+if %ERRORLEVEL% EQU 12 echo FAIL + MISMATCHES& goto end
+if %ERRORLEVEL% EQU 11 echo OKCOPY + FAIL + XTRA & goto end
+if %ERRORLEVEL% EQU 10 echo FAIL + XTRA & goto end
+if %ERRORLEVEL% EQU 9 echo OKCOPY + FAIL & goto end
+if %ERRORLEVEL% EQU 8 echo FAIL & goto end
+if %ERRORLEVEL% EQU 7 echo OKCOPY + MISMATCHES + XTRA & goto end
+if %ERRORLEVEL% EQU 6 echo MISMATCHES + XTRA & goto end
+if %ERRORLEVEL% EQU 5 echo OKCOPY + MISMATCHES & goto end
+if %ERRORLEVEL% EQU 4 echo MISMATCHES & goto end
+if %ERRORLEVEL% EQU 3 echo OKCOPY + XTRA & goto end
+if %ERRORLEVEL% EQU 2 echo XTRA & goto end
+if %ERRORLEVEL% EQU 1 echo OKCOPY & goto end
+if %ERRORLEVEL% EQU 0 echo No Change & goto end
 if errorlevel 1 exit 1
