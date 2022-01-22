c1f2cc575ce9ab762fd71eef3c7a109939f9d4bc
Uwe L. Korn <uwe.korn@quantco.com>
2022-01-22 21:56:49 +0100

    Delete the inner node_modules

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,5 +3,7 @@ COPY npm %PREFIX%\npm
 COPY npm.cmd %PREFIX%\npm.cmd
 COPY npx %PREFIX%\npx
 COPY npx.cmd %PREFIX%\npx.cmd
-@rem ROBOCOPY node_modules %PREFIX%\node_modules /s /e
+rd /s /q "node_modules\npm\node_modules"
+if NOT ERRORLEVEL 0 exit /b 1
+ROBOCOPY node_modules %PREFIX%\node_modules /s /e
 if %ERRORLEVEL% LSS 8 exit 0
