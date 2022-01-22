323ef20be577bc4ac67d3d2f1e16c1954bf248bc
Uwe L. Korn <uwe.korn@quantco.com>
2022-01-22 21:37:13 +0100

    Bisect other half of files

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
-@rem COPY node.exe %PREFIX%\node.exe
-@rem COPY npm %PREFIX%\npm
-@rem COPY npm.cmd %PREFIX%\npm.cmd
-@rem COPY npx %PREFIX%\npx
-@rem COPY npx.cmd %PREFIX%\npx.cmd
-ROBOCOPY node_modules %PREFIX%\node_modules /s /e
+COPY node.exe %PREFIX%\node.exe
+COPY npm %PREFIX%\npm
+COPY npm.cmd %PREFIX%\npm.cmd
+COPY npx %PREFIX%\npx
+COPY npx.cmd %PREFIX%\npx.cmd
+@rem ROBOCOPY node_modules %PREFIX%\node_modules /s /e
 if %ERRORLEVEL% LSS 8 exit 0
