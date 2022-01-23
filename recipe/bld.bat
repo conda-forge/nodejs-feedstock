3135ea50c4859d2a27cd10664063e683684477f1
Uwe L. Korn <uwe.korn@quantco.com>
2022-01-23 09:18:10 +0100

    Try implementing binary search

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,11 +3,16 @@ COPY npm %PREFIX%\npm
 COPY npm.cmd %PREFIX%\npm.cmd
 COPY npx %PREFIX%\npx
 COPY npx.cmd %PREFIX%\npx.cmd
-rd /s /q "node_modules\npm\node_modules\node-gyp"
-if NOT ERRORLEVEL 0 exit /b 1
-rd /s /q "node_modules\npm\node_modules\har-validator"
-if NOT ERRORLEVEL 0 exit /b 1
-rd /s /q "node_modules\npm\node_modules\bluebird"
-if NOT ERRORLEVEL 0 exit /b 1
+set /a COUNTER=1
+setlocal ENABLEDELAYEDEXPANSION
+for /D %%d in ("node_modules\npm\node_modules\*") do (
+  set /a COUNTER+=1
+  echo !COUNTER!
+  if !COUNTER! LEQ 10 (
+     echo %%d
+     rd /s /q "%%d"
+     if NOT ERRORLEVEL 0 exit 1
+  )
+)
 ROBOCOPY node_modules %PREFIX%\node_modules /s /e
 if %ERRORLEVEL% LSS 8 exit 0
