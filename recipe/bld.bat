6aa864870d99f2a748869c2d06383eaa57ee4063
Uwe L. Korn <uwe.korn@quantco.com>
2022-01-23 20:26:18 +0100

    Delete term-size vendor

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,18 +3,20 @@ COPY npm %PREFIX%\npm
 COPY npm.cmd %PREFIX%\npm.cmd
 COPY npx %PREFIX%\npx
 COPY npx.cmd %PREFIX%\npx.cmd
-set /a COUNTER=1
-setlocal ENABLEDELAYEDEXPANSION
-for /D %%d in ("node_modules\npm\node_modules\*") do (
-  set /a COUNTER+=1
-  echo !COUNTER!
-  if !COUNTER! LEQ 315 (
-     if !COUNTER! GEQ 315 (
-       echo %%d
-       rd /s /q "%%d"
-       if NOT ERRORLEVEL 0 exit 1
-     )
-  )
-)
+rd /s /q "node_modules/npm/node_modules/term-size/vendor/macos"
+if NOT ERRORLEVEL 0 exit 1
+@rem set /a COUNTER=1
+@rem setlocal ENABLEDELAYEDEXPANSION
+@rem for /D %%d in ("node_modules\npm\node_modules\*") do (
+@rem   set /a COUNTER+=1
+@rem   echo !COUNTER!
+@rem   if !COUNTER! LEQ 315 (
+@rem      if !COUNTER! GEQ 315 (
+@rem        echo %%d
+@rem        rd /s /q "%%d"
+@rem        if NOT ERRORLEVEL 0 exit 1
+@rem      )
+@rem   )
+@rem )
 ROBOCOPY node_modules %PREFIX%\node_modules /s /e
 if %ERRORLEVEL% LSS 8 exit 0
