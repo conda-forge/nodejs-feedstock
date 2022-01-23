6179da0baa731e4d556b1632966f5bae577ac6fa
Uwe L. Korn <uwe.korn@quantco.com>
2022-01-23 14:09:10 +0100

    Start binary search at 2000

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,7 @@ setlocal ENABLEDELAYEDEXPANSION
 for /D %%d in ("node_modules\npm\node_modules\*") do (
   set /a COUNTER+=1
   echo !COUNTER!
-  if !COUNTER! LEQ 10 (
+  if !COUNTER! LEQ 2000 (
      echo %%d
      rd /s /q "%%d"
      if NOT ERRORLEVEL 0 exit 1
