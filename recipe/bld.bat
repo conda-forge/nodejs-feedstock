8ba2480ffdc4f92b1b96415d23ddb52638aa8cf7
Uwe L. Korn <uwe.korn@quantco.com>
2022-01-23 19:25:53 +0100

    GEQ 315, LEQ 317

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,7 @@ setlocal ENABLEDELAYEDEXPANSION
 for /D %%d in ("node_modules\npm\node_modules\*") do (
   set /a COUNTER+=1
   echo !COUNTER!
-  if !COUNTER! LEQ 320 (
+  if !COUNTER! LEQ 317 (
      if !COUNTER! GEQ 315 (
        echo %%d
        rd /s /q "%%d"
