0f67baa99ce9cceb107dc4c14a4a05fbb1ca0567
Uwe L. Korn <uwe.korn@quantco.com>
2022-01-23 08:48:04 +0100

    Delete in more detail

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,11 @@ COPY npm %PREFIX%\npm
 COPY npm.cmd %PREFIX%\npm.cmd
 COPY npx %PREFIX%\npx
 COPY npx.cmd %PREFIX%\npx.cmd
-rd /s /q "node_modules\npm\node_modules"
+rd /s /q "node_modules\npm\node_modules\node-gyp"
+if NOT ERRORLEVEL 0 exit /b 1
+rd /s /q "node_modules\npm\node_modules\har-validator"
+if NOT ERRORLEVEL 0 exit /b 1
+rd /s /q "node_modules\npm\node_modules\bluebird"
 if NOT ERRORLEVEL 0 exit /b 1
 ROBOCOPY node_modules %PREFIX%\node_modules /s /e
 if %ERRORLEVEL% LSS 8 exit 0
