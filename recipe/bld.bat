a0f9ba060fbf2a6b43985ae67501594faee26315
Uwe L. Korn <uwe.korn@quantco.com>
2022-01-23 15:49:06 +0100

    GEQ 315, LEQ 337

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,10 +8,12 @@ setlocal ENABLEDELAYEDEXPANSION
 for /D %%d in ("node_modules\npm\node_modules\*") do (
   set /a COUNTER+=1
   echo !COUNTER!
-  if !COUNTER! GEQ 337 (
-     echo %%d
-     rd /s /q "%%d"
-     if NOT ERRORLEVEL 0 exit 1
+  if !COUNTER! LEQ 337 (
+     if !COUNTER! GEQ 315 (
+       echo %%d
+       rd /s /q "%%d"
+       if NOT ERRORLEVEL 0 exit 1
+     )
   )
 )
 ROBOCOPY node_modules %PREFIX%\node_modules /s /e
