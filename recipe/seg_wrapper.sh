21df25fce48c34f17701c8c2de91822c5f1da228
Uwe Korn <uwelk@xhochy.com>
2022-09-30 14:19:06 +0000

    Try using libSegFault

--- recipe/seg_wrapper.sh
+++ recipe/seg_wrapper.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 ulimit -c unlimited
-gdb -x $(pwd)/backtrace --args "$@"
-#if [[ $? -eq 139 ]]; then
-#  ls -l
-#  file $1
-#  gdb -q $1 *.core -x $(pwd)/backtrace
-#fi
+ls -l /lib/*/libSeg*.so
+LD_PRELOAD=/lib/powerpc64le-linux-gnu/libSegFault.so "$@"
+if [[ $? -eq 139 ]]; then
+  ls -l
+  file $1
+  gdb -q $1 *.core -x $(pwd)/backtrace
+fi
