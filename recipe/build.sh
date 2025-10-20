901661e796aff55a0bafe1a09825591ce6970ffc
Uwe L. Korn <uwe.korn@quantco.com>
2025-10-20 14:37:38 +0200

    Use correct bash pattern

--- recipe/build.sh
+++ recipe/build.sh
@@ -90,7 +90,7 @@ if [[ "${target_platform}" != "${build_platform}" ]]; then
   # Don't optimize code for the build host so much
   for ninja_build in `find out/Release/obj.host/ -name '*.ninja'`; do
     # O0 leads to compilation issues with GCC
-    if [[ "${target_platform}" == "linux-*" ]]; then
+    if [[ "${target_platform}" == linux-* ]]; then
       sed -ie 's/-O3/-O1/g' ${ninja_build}
     else
       sed -ie 's/-O3/-O0/g' ${ninja_build}
