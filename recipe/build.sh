b37ba93f7f43c9e4197a75485c3383b242222e07
velonica0 <like@mail.nankai.edu.cn>
2026-08-23 18:46:25 -0700

    Support cross-compiling to linux-riscv64

--- recipe/build.sh
+++ recipe/build.sh
@@ -34,6 +34,9 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
         ppc64le)
            DEST_ARCH=ppc64
            ;;
+        riscv64)
+           DEST_ARCH=riscv64
+           ;;
         *)
            echo "unknown architecture for cross"
            exit 1
